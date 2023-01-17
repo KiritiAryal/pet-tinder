96ffe884a4e0be0d0a3fab137ea35f9b09f16f78
KiritiAryal <karyal@ttu.edu>
2023-01-17 14:10:47 -0600

    Initial vite, yarn commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return <p>Welcome to Pet Tinder</p>;
+}
+
+export default App;
